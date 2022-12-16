ad6d5dedf139a10db4216fd13c7d94a0f14f303e
Sophie Chen <sophie.chen@anu.edu.au>
2022-12-16 15:38:09 +1100

    Tests for Fibonacci package

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,8 @@
+"""Tests for function fib in fibonacci package."""
+from fibonacci import fib
+
+def test_fibonacci_values():
+
+    for i, f in enumerate[1,1,2,3,5,8]:
+        assert fib(i+1) == f
+
